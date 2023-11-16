a0f264595d8d1b57f46e653e784e32105837fd12
Alex Platas <48363345+alexanderplatas@users.noreply.github.com>
2023-11-16 20:45:54 +0100

    Update algorithm.py

--- algorithm.py
+++ algorithm.py
@@ -1,4 +1,5 @@
 from MazeEnv import MazeEnv
+import pygame
 
 env = MazeEnv(initial_state=(0, 0), final_state=(7, 7),
               obstacles=[(4, 0), (4, 1), (4, 2), (4, 3), (4, 4), (6, 7), (6, 6), (6, 5), (6, 4), (6, 3)])
@@ -7,16 +8,17 @@ state, reward, done = env.reset()
 
 dic = {'a': 3, 'w': 0, 's': 1, 'd': 2}
 
-env.render()
+env.render_human()
 
 while not done:
 
-    action = input("ACTION: \n")
+    action = input()
     if action in dic:
         next_state, reward, done = env.step(dic[action])
-        env.render()
+        env.render_human()
         state = next_state
-
+#env.action_log = [2, 2, 2, 1, 1, 1, 2, 2, 1, 1, 3, 3, 3, 1, 1, 2, 2, 2, 2, 2]
 print(f"Action log: {env.action_log}")
+env.render()
 
 env.close()
