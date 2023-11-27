2e43c8cc67531353f3a929a71bc07e88638b114f
Alex Platas <48363345+alexanderplatas@users.noreply.github.com>
2023-11-27 16:52:30 +0100

    Update MazeEnv.py

--- MazeEnv.py
+++ MazeEnv.py
@@ -1,33 +1,42 @@
+import json
+from random import randint
+from time import sleep
+
+import cv2
 import gymnasium
-import matplotlib.pyplot as plt
 from gym import spaces
 from gymnasium import spaces
-from matplotlib import colors
 from pylab import *
-from time import sleep
-import pygame
-import cv2
-from random import randint
 
-# Dimensiones de la ventana y del tablero
-WIDTH, HEIGHT = 400, 400
-DIMENSION = 8
-SQUARE_SIZE = HEIGHT // DIMENSION
+MAX_STEPS_LIMIT = 100
+with open('environment.json', 'r', encoding='utf8') as file:
+    DATA = json.load(file)
 
-# Colores
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
+def random_final_state(obstacles, initial_state):
+    # Generar un destino random
+    final_state = (randint(0, 7), randint(0, 7))
+    while final_state in obstacles or final_state == initial_state:
+        final_state = (randint(0, 7), randint(0, 7))
+    return final_state
 
 
 class MazeEnv(gymnasium.Env):
 
-    def __init__(self, initial_state, final_state, obstacles, render):
+    def __init__(self, render):
 
         super(MazeEnv, self).__init__()
+
+        # Estado inicial
+        self.initial_state = DATA['initial_state']
+
+        # Obstáculos
+        obstacles = DATA['obstacles']
+
+        # self.final_state = DATA['final_state']
+        self.final_state = random_final_state(obstacles, self.initial_state)
+
         self.action_log = []
-        self.position_log = set(initial_state)
+        self.position_log = set(self.initial_state)
 
         # Definimos el espacio de acción
         #   0: UP
@@ -38,154 +47,180 @@ class MazeEnv(gymnasium.Env):
         self.observation_space = spaces.Box(low=0, high=80,
                                             shape=(4,), dtype=np.float64)
 
-        self.initial_state = initial_state
-        self.final_state = self.random_final_state(obstacles, initial_state)
-
-        # Duración de la ducha en segundos
-        self.max_steps = 64
-
+        # Lista de coordenadas de los obstaculos
         self.obstacles = obstacles
 
-        self.renderize = render
+        # Visualización del juego
+        self.render = render
 
-        # Contador de tiempo
-        self.current_step = 0
-
+        # Fin de la partida
         self.truncated = False
+        self.done = False
 
         # Actual state (fila, columna)
-        self.current_state = initial_state
+        self.current_state = self.initial_state
         self.next_state = (0, 0)
 
+        # Número de pasos
+        self.num_steps = 0
+
+        # Recompensa
         self.reward = 0
 
-        if self.renderize:
+        if self.render:
 
-            ## BOARD ##
-            self.img =np.zeros((640, 640, 3), dtype=np.uint8)
+            # Draw board (black and white)
+            self.img = np.zeros((640, 640, 3), dtype=np.uint8)
             for x in range(0, 9):
-                if x % 2 == 0: p = range(0, 8, 2)
-                else: p = range(1, 9, 2)
+                if x % 2 == 0:
+                    p = range(0, 8, 2)
+                else:
+                    p = range(1, 9, 2)
                 for y in p:
-                    cv2.rectangle(self.img, (x*80, y*80), (x*80+80,y*80+80), (255,255,255), -1)       
+                    cv2.rectangle(self.img, (x * 80, y * 80), (x * 80 + 80, y * 80 + 80), (255, 255, 255), -1)
 
-            # OBSTACLES
+            # Draw obstacles (red)
             for obstacle in obstacles:
-                cv2.rectangle(self.img, (obstacle[0]*80, obstacle[1]*80),
-                            (obstacle[0]*80+80,obstacle[1]*80+80), (0,0,255), -1)
-                
-            # GOAL
-            cv2.rectangle(self.img, (self.final_state[0]*80, self.final_state[1]*80),
-                            (self.final_state[0]*80+80,self.final_state[1]*80+80), (0,255,255), -1)
-        
+                cv2.rectangle(self.img, (obstacle[0] * 80, obstacle[1] * 80),
+                              (obstacle[0] * 80 + 80, obstacle[1] * 80 + 80), (0, 0, 255), -1)
 
-    def random_final_state(self, obstacles, initial_state):
-
-        final_state = (randint(0,7), randint(0,7))
-        while final_state in obstacles or final_state == initial_state:
-            final_state = (randint(0,7), randint(0,7))
-        return final_state
-
-    def is_posible(self):
-
-        return self.next_state not in self.obstacles and -1 < self.next_state[0] < 8 and -1 < self.next_state[1] < 8
-
-    def is_terminal(self):
-
-        return self.final_state[0] == self.current_state[0] and self.final_state[1] == self.current_state[1]
+            # Draw goal (yellow)
+            cv2.rectangle(self.img, (self.final_state[0] * 80, self.final_state[1] * 80),
+                          (self.final_state[0] * 80 + 80, self.final_state[1] * 80 + 80), (0, 255, 255), -1)
 
     def step(self, action):
+
+        info = {"Solution": None}
+
+        ############## Mover ##############
 
         if action == 0: self.next_state = (self.current_state[0] - 1, self.current_state[1])
         if action == 1: self.next_state = (self.current_state[0] + 1, self.current_state[1])
         if action == 2: self.next_state = (self.current_state[0], self.current_state[1] + 1)
         if action == 3: self.next_state = (self.current_state[0], self.current_state[1] - 1)
 
-        # print(f'Distancia goal: {np.sum(np.abs(np.array(self.current_state) - np.array(self.final_state)))}')
-        if self.is_posible():
+        if self._is_posible(self.next_state):
 
             self.action_log.append(action)
             self.current_state = self.next_state
-            self.current_step += 1
+            self.num_steps += 1
+            goal_reward = 0
 
-            self.reward = 15 - np.sum(np.abs(np.array(self.current_state) - np.array(self.final_state)))
+            ########## Visualización ##########
 
-            if self.current_state in self.position_log:
-                self.reward -= 1
+            if self.render:
+                board = self.img.copy()
+                cv2.circle(board, (self.current_state[0] * 80 + 40, self.current_state[1] * 80 + 40), 30, (0, 255, 0), -1)
+                cv2.imshow('DUMB MAZE RUNNER', board)
+                cv2.waitKey(1)
+                sleep(0.2)
 
+            if self._is_terminal(self.next_state):
 
-        else:
-            #print('ILEGAL ACTION\t')
+                if self.render:
+                    board = np.zeros((640, 640, 3), dtype=np.uint8)
+                    cv2.putText(board, f'You Win! ({self.num_steps} steps)', (200, 320), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+                    cv2.imshow('DUMB MAZE RUNNER', board)
+                    cv2.waitKey(1)
+                    sleep(1)
+
+                info["Solution"] = self.action_log
+                goal_reward = 100
+                self.done = True
+
+            ############# Rewards #############
+            manhattan_dist_to_goal = 0.001 + np.sum(np.abs(np.array(self.current_state) - np.array(self.final_state)))
+            self.reward = (64 - manhattan_dist_to_goal + goal_reward) / self.num_steps
+
+        # else:
+        #     print('ILEGAL ACTION\t')
+        #     self.reward = -10
+        #     self.truncated = True
+        #     self.done = True
+
+        ######## Nueva observación ########
+
+        if self.num_steps >= MAX_STEPS_LIMIT:
+            self.done = True
+            self.truncated = True
             self.reward = -100
-            # self.truncated = True
-
-
-        ########## Visualización ##########
-
-        
-
-        if self.is_terminal():
-
-            board = np.zeros((640, 640, 3), dtype=np.uint8)
-            cv2.putText(board, 'You Win!', (280, 320), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
-            self.reward = 100
-            sleep_time = 1
-            # TODO return action log
-        
-        else: 
-
-            board = self.img.copy()
-            cv2.circle(board, (self.current_state[0]*80+40, self.current_state[1]*80+40), 30, (0,255,0), -1)
-            sleep_time = 0.05
-
-        if self.renderize:
-            cv2.imshow('DUMB MAZE RUNNER', board)
-            cv2.waitKey(1)
-            sleep(sleep_time)
-
-        ###################################   
 
         obs = np.array([self.current_state[0], self.current_state[1],
-                        abs(self.current_state[0]-self.final_state[0]),
-                        abs(self.current_state[1]-self.final_state[1])])
-        
-        return obs, self.reward, self.is_terminal(), self.truncated, {}
+                        abs(self.current_state[0] - self.final_state[0]),
+                        abs(self.current_state[1] - self.final_state[1])])
 
-    def reset(self):
+        return obs, self.reward, self.done, self.truncated, info
 
-        self.current_step = 0
-        self.current_state = (0, 0)
-        self.reward = 0
+    def reset(self, seed=None):
+
+
+
+        # Estado inicial
+        self.initial_state = DATA['initial_state']
+
+        # Obstáculos
+        obstacles = DATA['obstacles']
+
+        # self.final_state = DATA['final_state']
+        self.final_state = random_final_state(obstacles, self.initial_state)
+
         self.action_log = []
-        self.final_state = self.random_final_state(self.obstacles, self.initial_state)
+        self.position_log = set(self.initial_state)
 
-        self.img =np.zeros((640, 640, 3), dtype=np.uint8)
+        # Definimos el espacio de acción
+        #   0: UP
+        #   1: DOWN
+        #   2: RIGHT
+        #   3: LEFT
+        self.action_space = spaces.Discrete(4)
+        self.observation_space = spaces.Box(low=0, high=80,
+                                            shape=(4,), dtype=np.float64)
 
-        if self.renderize:
+        # Lista de coordenadas de los obstaculos
+        self.obstacles = obstacles
 
-            ## BOARD ##
-            
+        # Fin de la partida
+        self.truncated = False
+        self.done = False
+
+        # Actual state (fila, columna)
+        self.current_state = self.initial_state
+        self.next_state = (0, 0)
+
+        # Número de pasos
+        self.num_steps = 0
+
+        # Recompensa
+        self.reward = 0
+
+        if self.render:
+
+            # Draw board (black and white)
+            self.img = np.zeros((640, 640, 3), dtype=np.uint8)
             for x in range(0, 9):
-                if x % 2 == 0: p = range(0, 8, 2)
-                else: p = range(1, 9, 2)
+                if x % 2 == 0:
+                    p = range(0, 8, 2)
+                else:
+                    p = range(1, 9, 2)
                 for y in p:
-                    cv2.rectangle(self.img, (x*80, y*80), (x*80+80,y*80+80), (255,255,255), -1)       
-            ###############
+                    cv2.rectangle(self.img, (x * 80, y * 80), (x * 80 + 80, y * 80 + 80), (255, 255, 255), -1)
 
-            for obstacle in self.obstacles:
-                cv2.rectangle(self.img, (obstacle[0]*80, obstacle[1]*80),
-                            (obstacle[0]*80+80,obstacle[1]*80+80), (0,0,255), -1)
-            cv2.rectangle(self.img, (self.final_state[0]*80, self.final_state[1]*80),
-                            (self.final_state[0]*80+80,self.final_state[1]*80+80), (0,255,255), -1)
+            # Draw obstacles (red)
+            for obstacle in obstacles:
+                cv2.rectangle(self.img, (obstacle[0] * 80, obstacle[1] * 80),
+                              (obstacle[0] * 80 + 80, obstacle[1] * 80 + 80), (0, 0, 255), -1)
 
-        info = {}
-
-        obs = np.array([self.current_state[0], self.current_state[1],
-                        abs(self.current_state[0]-self.final_state[0]),
-                        abs(self.current_state[1]-self.final_state[1])])
+            # Draw goal (yellow)
+            cv2.rectangle(self.img, (self.final_state[0] * 80, self.final_state[1] * 80),
+                          (self.final_state[0] * 80 + 80, self.final_state[1] * 80 + 80), (0, 255, 255), -1)
 
         return obs, info
 
+    def _is_posible(self, state):
+        return state not in self.obstacles and -1 < state[0] < 8 and -1 < state[1] < 8
+
+    def _is_terminal(self, state):
+        return self.final_state[0] == state[0] and self.final_state[1] == state[1]
 
     def close(self):
         pass
